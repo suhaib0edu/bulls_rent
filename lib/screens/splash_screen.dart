cb3cd0776b694ab244e6882b419ad8d5c234cd9b
Suhaib Eltayeb <suhab.edu@gmail.com>
2022-12-08 22:01:18 +0200

    Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -9,7 +9,30 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {});
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(height: 30,),
+            Image.network('https://purepng.com/public/uploads/large/purepng.com-yellow-lexus-lfa-carcarvehicletransportlexus-961524667139almse.png',scale: 5,),
+            SizedBox(height: 30,),
+            Text('Bull`s\nnrent',style: TextStyle(fontSize: 70,fontWeight: FontWeight.w800),),
+            CircularProgressIndicator(
+              color: Colors.black,
+            ),
+            Text('Bull`s nrent',style: TextStyle(fontSize: 12,fontWeight: FontWeight.w800),),
+          ],
+        ),
+      )
+    );
   }
 }
